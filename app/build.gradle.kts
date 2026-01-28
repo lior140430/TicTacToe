63ece983061458b40fa7292abd1fc09e43770390
rony eshel <Eshelrony@gmail.com>
2026-01-28 22:31:11 +0200

    fix (wrong kotlin config)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -26,6 +27,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
     }
 }
 
